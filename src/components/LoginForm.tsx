018e77e70c53dfd7a699a98049c1dd9e7d53f3e4
RSLastra <raul.slastra@gmail.com>
2024-11-17 22:54:27 -0600

    style(loginForm): change style

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -3,8 +3,9 @@
 export const LoginForm = () => {
   return (
     <form className="mx-auto">
-      <div className="mb-5">
-        <label htmlFor="email" className="block mb-2 text-sm font-medium  text-white">email</label>
+      <h2 className="text-white text-center text-2xl">Iniciar Sesión</h2>
+      <div className="my-5">
+        <label htmlFor="email" className="block mb-2 text-sm font-medium  text-white">correo electronico</label>
         <input type="email" id="email" className=" border text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@correo.com" required />
       </div>
       <div className="mb-5">
@@ -12,13 +13,14 @@ export const LoginForm = () => {
         <input type="email" id="email" className=" border text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" required />
       </div>
 
-      <div className="flex items-start mb-5">
-        <div className="flex items-center h-5">
-          <input id="remember" type="checkbox" value="" className="w-4 h-4 border rounded  focus:ring-3 bg-gray-700 border-gray-600 focus:ring-blue-600 ring-offset-gray-800 focus:ring-offset-gray-800" required />
-        </div>
-        <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-300">Remember me</label>
+      <div className="flex items-end mb-5">
+        <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-300">
+          Registrarse
+        </label>
       </div>
-      <button type="submit" className="text-white focus:ring-4 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">iniciar sesión</button>
+      <button type="submit" className="text-white focus:ring-4 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center bg-purple-800 hover:bg-purple-900 focus:ring-blue-800">
+        iniciar sesión
+      </button>
     </form>
   )
 }
